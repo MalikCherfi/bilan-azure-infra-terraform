16cdd2a22c38ad07c56c45a3f2357a290323b574
malikcherfi <malikcherfi@gmail.com>
2026-08-06 14:01:05 +0200

    Update Helm provider configuration to use kube_admin_config for AKS cluster

--- main.tf
+++ main.tf
@@ -22,10 +22,10 @@ data "azurerm_kubernetes_cluster" "shared" {
 
 provider "helm" {
   kubernetes = {
-    host                   = data.azurerm_kubernetes_cluster.shared.kube_config.0.host
-    client_certificate     = base64decode(data.azurerm_kubernetes_cluster.shared.kube_config.0.client_certificate)
-    client_key             = base64decode(data.azurerm_kubernetes_cluster.shared.kube_config.0.client_key)
-    cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.shared.kube_config.0.cluster_ca_certificate)
+    host                   = data.azurerm_kubernetes_cluster.shared.kube_admin_config.0.host
+    client_certificate     = base64decode(data.azurerm_kubernetes_cluster.shared.kube_admin_config.0.client_certificate)
+    client_key             = base64decode(data.azurerm_kubernetes_cluster.shared.kube_admin_config.0.client_key)
+    cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.shared.kube_admin_config.0.cluster_ca_certificate)
   }
 }
 
