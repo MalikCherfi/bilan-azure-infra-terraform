fcaf1aec3d3d46ac11679ec5a74d05f0c0690172
malikcherfi <malikcherfi@gmail.com>
2026-09-22 14:47:06 +0200

    Fix module source path for PostgreSQL in main.tf

--- main.tf
+++ main.tf
@@ -61,7 +61,7 @@ module "container-registry" {
 }
 
 module "psql" {
-  source = "./modules/postgresql"
+  source = "./modules/psql"
 
   resource_group_name    = var.resource_group_name
   location               = var.location
