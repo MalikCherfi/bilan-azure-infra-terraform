ab82c88d8fd6bdd767677172538a98b5d34f8947
malikcherfi <malikcherfi@gmail.com>
2026-09-24 09:45:11 +0200

    Remove unused keyvault_access_policy output from outputs.tf

--- modules/keyvault/outputs.tf
+++ modules/keyvault/outputs.tf
@@ -1,11 +1,6 @@
 output "keyvault_id" {
   value = azurerm_key_vault.keyvault.id
 }
-
-output "keyvault_access_policy" {
-  value = azurerm_key_vault_access_policy.keyvault_access_policy
-}
-
 output "wait_for_access_policy" {
   value = time_sleep.wait_for_access_policy
 }
